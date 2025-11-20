2702faa1d0edf9fa05466bc6e9e57ea763777057
Ivan Levshyn <Ivan.Levshyn@go-ecommerce.de>
2025-11-20 08:40:08 +0100

    Add environment variables for n8n host and protocol in docker-compose.yml
    - Introduced N8N_HOST and N8N_PROTOCOL variables to configure the n8n service for a specific host and secure connection.
    - Updated server.py to include age-based user classification, enhancing user interaction with age prompts and responses.

--- server.py
+++ server.py
@@ -1,1 +1,20 @@
 print  ("hallo")
+age = int(input("Bitte Alter eingeben: ")) 
+
+ 
+
+if age >= 18: 
+
+    print("Du bist volljährig.") 
+
+ 
+
+if age >= 13: 
+
+    print("Du bist ein Teenager.") 
+
+ 
+
+if age <= 12: 
+
+    print("Du bist ein Kind.") 
