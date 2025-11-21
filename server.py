1772fa516bac37dd4e1a32af9307c8f747efa992
Ivan Levshyn <Ivan.Levshyn@go-ecommerce.de>
2025-11-21 10:36:50 +0100

    Remove DEPLOY.md and update docker-compose.yml to enhance n8n setup
    - Deleted the DEPLOY.md file to streamline documentation and reduce redundancy.
    - Updated docker-compose.yml to introduce n8n-init and auto-login-proxy services, improving user initialization and login processes.
    - Adjusted service dependencies to ensure proper startup sequence and added relevant environment variables for enhanced configuration.

--- server.py
+++ server.py
@@ -2,19 +2,3 @@ print  ("hallo")
 age = int(input("Bitte Alter eingeben: ")) 
 
  
-
-if age >= 18: 
-
-    print("Du bist volljährig.") 
-
- 
-
-if age >= 13: 
-
-    print("Du bist ein Teenager.") 
-
- 
-
-if age <= 12: 
-
-    print("Du bist ein Kind.") 
